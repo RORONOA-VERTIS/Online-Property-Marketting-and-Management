869580072de4e0cafe530122dc51bc5c5310ad8a
RORONOA-VERTIS <94117316+RORONOA-VERTIS@users.noreply.github.com>
2024-05-05 22:30:57 +0530

    Add files via upload

---
+++ homeverse-master/homeverse-master/assets/js/script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+const navElemArr = [overlay, navCloseBtn, navOpenBtn];
+
+/**
+ * close navbar when click on any navbar link
+ */
+
+for (let i = 0; i < navbarLinks.length; i++) { navElemArr.push(navbarLinks[i]); }
+
+/**
+ * addd event on all elements for toggling navbar
+ */
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+  });
+}
+
+const loginIcon = document.querySelector(".header-bottom-actions-btn]");
+const loginForm = document.querySelector("#login-container"); // Assuming the ID of your login form is "login-form"
+
+loginIcon.addEventListener("click", function() {
+  // Check if the login form is currently visible
+  const isVisible = loginForm.classList.contains("active");
+
+  // Toggle the visibility of the login form
+  elemToggleFunc(loginForm);
+
+  // If the form was already visible, close it on clicking the icon again
+  if (isVisible) {
+    elemToggleFunc(loginForm);
+  }
+});
+
+
+
+/**
+ * header active state
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 400 ? header.classList.add("active")
+    : header.classList.remove("active");
+}); 
